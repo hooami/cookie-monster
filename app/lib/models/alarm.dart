ea6009a63b214b5b2a7c69c6c077119a64e970be
Jonoans <jonjontan610@hotmail.com>
2025-01-18 20:02:06 +0800

    Add model and meetup page service

---
+++ app/lib/models/alarm.dart
@@ -0,0 +1,68 @@
+import 'package:app/models/db_constants.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class AlarmModel {
+  String uuid;
+  String groupId;
+  String meetupId;
+  String userId;
+  String datetime;
+  String sound;
+  int snoozeCount;
+  bool turnedOff;
+
+  AlarmModel({
+    required this.uuid,
+    required this.groupId,
+    required this.meetupId,
+    required this.userId,
+    required this.datetime,
+    this.sound = "",
+    this.snoozeCount = 0,
+    this.turnedOff = false,
+  });
+
+  Future<void> insert() async {
+    await DbConstants.connect();
+    await DbConstants.alarms.insert(toMap());
+  }
+
+  static Future<List<AlarmModel?>> getAlarmsByGroup(String groupId) async {
+    await DbConstants.connect();
+    return await DbConstants.alarms
+        .find(where.eq("groupId", groupId))
+        .map((alarm) {
+      return _toModel(alarm);
+    }).toList();
+  }
+
+  static AlarmModel? _toModel(Map<String, dynamic>? alarmMap) {
+    if (alarmMap == null) {
+      return null;
+    }
+
+    return AlarmModel(
+      uuid: alarmMap["uuid"],
+      groupId: alarmMap["groupId"],
+      meetupId: alarmMap["meetupId"],
+      userId: alarmMap["userId"],
+      datetime: alarmMap["datetime"],
+      sound: alarmMap["sound"],
+      snoozeCount: alarmMap["snoozeCount"],
+      turnedOff: alarmMap["turnedOff"],
+    );
+  }
+
+  toMap() {
+    return {
+      uuid: uuid,
+      groupId: groupId,
+      meetupId: meetupId,
+      userId: userId,
+      datetime: datetime,
+      sound: sound,
+      snoozeCount: snoozeCount,
+      turnedOff: turnedOff,
+    };
+  }
+}
--- app/lib/models/db_constants.dart
+++ app/lib/models/db_constants.dart
@@ -4,8 +4,10 @@ class DbConstants {
   static String dbUrl = "";
 
   static Db? db;
+  static final DbCollection alarms = db!.collection("alarms");
   static final DbCollection groups = db!.collection("groups");
   static final DbCollection users = db!.collection("users");
+  static final DbCollection meetups = db!.collection("meetups");
 
   static Future<void> connect() async {
     db = await Db.create(DbConstants.dbUrl);
