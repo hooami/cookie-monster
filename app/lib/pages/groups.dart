ef254bf3c603f6c7addd468843d40130ad2c9f39
Jonoans <jonjontan610@hotmail.com>
2025-01-18 11:09:03 +0800

    Add search text field

--- app/lib/pages/groups.dart
+++ app/lib/pages/groups.dart
@@ -5,7 +5,25 @@ class Groups extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TopBar(index: 0),
-    );
+        appBar: TopBar(index: 0),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromRGBO(249, 249, 254, 1),
+                  // fillColor: Colors.black,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: 'Search',
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
